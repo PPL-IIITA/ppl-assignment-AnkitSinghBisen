46104b08f978d080d7b635de2f35daa018047686
AnkitSinghBisen <ankitsinghbisen96@gmail.com>
2017-02-27 06:48:59 +0530

    first commit

---
+++ Boy.java
@@ -0,0 +1,13 @@
+
+// ----- THIS CLASS CONTAINS ATTRIBUTES OF A BOY
+
+public class Boy {
+
+    String name;
+    float attractiveness;       //On a scale of 1-10
+    int budget;
+    float intelligence;         //On a scale of 1-10
+    float minatreq;             //On a scale of 1-10
+    char relstat;               //Relationship Status. S-SINGLE , C-COMMITTED
+
+}
